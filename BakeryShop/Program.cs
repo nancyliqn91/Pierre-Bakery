1735067848dc76df6415f36dd67dfb0cfc9a40c4
nancyliqn91 <nancyliqn91@gmail.com>
2023-05-05 11:55:44 -0700

    WIP for UI

---
+++ BakeryShop/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using BakeryShop.Models;
+
+class Program
+{
+  static void Main()
+  {
+    // UI
+    Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+    Console.WriteLine("Welcome to the Pierre's Bakery");
+    Console.WriteLine("We have breads($5 per loaf), and pastry($2 for each). please type the amount of breads you want.");
+    
+    
+
+
+
+
+  }
+}
