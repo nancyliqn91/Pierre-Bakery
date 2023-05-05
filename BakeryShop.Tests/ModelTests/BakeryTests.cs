2789d2372b425eaa705b9e47f205a1ce566d0428
nancyliqn91 <nancyliqn91@gmail.com>
2023-05-05 15:03:35 -0700

    Add 6th test

--- BakeryShop.Tests/ModelTests/BakeryTests.cs
+++ BakeryShop.Tests/ModelTests/BakeryTests.cs
@@ -53,12 +53,12 @@ namespace BakeryShop.Tests
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
 
-    // [TestMethod]
-    // public void BakeryOrder_ReturnBreadCost_Int()
-    // {
-    //   Bread newBread = new Bread(7);
-    //   Assert.AreEqual(25, newBread.BakeryOrder());
-    // }
+    [TestMethod]
+    public void BakeryOrder_ReturnPastryCost_Int()
+    {
+      Pastry newPastry = new Pastry(8);
+      Assert.AreEqual(12, newPastry.BakeryOrder());
+    }
   }
   // [TestClass]
   // public class BakeryTests
