bf1a53bf21cd209f84261c272bed0a7f939191ff
nancyliqn91 <nancyliqn91@gmail.com>
2023-05-05 09:35:49 -0700

    Pass first test

---
+++ BakeryShop/Models/Bakery.cs
@@ -0,0 +1,24 @@
+namespace BakeryShop.Models
+{
+  public class Bread
+  {
+    public int Loaves { get; set; }
+    // private int _breadMoney;
+    // Public int BreadMoney
+    // {
+    //   get { return _breadMoney; }
+    // }
+
+    public Bread(int bNum)
+    {
+       Loaves = bNum;
+    }
+
+  }
+
+
+
+
+
+
+}
