eff84ad2986a0747cf6bad4296a687148dac7b7b
nancyliqn91 <nancyliqn91@gmail.com>
2023-05-05 14:00:15 -0700

    Pass first test for class BakeryItem

--- BakeryShop/Models/Bakery.cs
+++ BakeryShop/Models/Bakery.cs
@@ -2,57 +2,83 @@ using System;
 
 namespace BakeryShop.Models
 {
-  public class Bread
+  public class BakeryItem
   {
-    public int Loaves { get; set; }
-    // private int _breadMoney;
-    public Bread(int bNum)
+    public int Quantity { get; set; }
+    public BakeryItem(int num)
     {
-       Loaves = bNum;
+      Quantity = num;
     }
 
-    public int BreadOrder()
-    {
-      int breadMoney = 0;
-      if (1 <= Loaves && Loaves< 3)
-      {
-        breadMoney = Loaves * 5;
-        return breadMoney;
-      }
-
-      if (Loaves >= 3)
-      {
-        breadMoney = Loaves * 5 - (int)(Math.Floor((double)(Loaves / 3))) * 5;
-        return breadMoney;
-      }
-
-      return breadMoney;
-    }
+    // public int BreadOrder()
+    // {
+    //   int breadMoney = 0;
+    //   if (1 <= Loaves && Loaves< 3)
+    //   {
+    //     breadMoney = Loaves * 5;
+    //     return breadMoney;
+    //   }
+    //   if (Loaves >= 3)
+    //   {
+    //     breadMoney = Loaves * 5 - (int)(Math.Floor((double)(Loaves / 3))) * 5;
+    //     return breadMoney;
+    //   }
+ 
+    //   return breadMoney;
+    // }
   }
 
-  public class Pastry
-  {
-    public int Loaves { get; set; }
-    public Pastry(int pNum)
-    {
-       Loaves = pNum;
-    }
 
-    public int PastryOrder()
-    {
-      int pastryMoney = 0;
-      if (1 <= Loaves && Loaves< 4)
-      {
-        pastryMoney = Loaves * 2;
-        return pastryMoney;
-      }
-      if (Loaves >= 4)
-      {
-        pastryMoney = Loaves * 2 - (int)(Math.Floor((double)(Loaves / 4))) * 2;
-        return pastryMoney;
-      }
-      return pastryMoney;
-    }
-  }
+  // public class Bread
+  // {
+  //   public int Loaves { get; set; }
+  //   // private int _breadMoney;
+  //   public Bread(int bNum)
+  //   {
+  //     Loaves = bNum;
+  //   }
+
+  //   public int BreadOrder()
+  //   {
+  //     int breadMoney = 0;
+  //     if (1 <= Loaves && Loaves< 3)
+  //     {
+  //       breadMoney = Loaves * 5;
+  //       return breadMoney;
+  //     }
+  //     if (Loaves >= 3)
+  //     {
+  //       breadMoney = Loaves * 5 - (int)(Math.Floor((double)(Loaves / 3))) * 5;
+  //       return breadMoney;
+  //     }
+
+  //     return breadMoney;
+  //   }
+  // }
+
+  // public class Pastry
+  // {
+  //   public int Loaves { get; set; }
+  //   public Pastry(int pNum)
+  //   {
+  //      Loaves = pNum;
+  //   }
+
+  //   public int PastryOrder()
+  //   {
+  //     int pastryMoney = 0;
+  //     if (1 <= Loaves && Loaves< 4)
+  //     {
+  //       pastryMoney = Loaves * 2;
+  //       return pastryMoney;
+  //     }
+  //     if (Loaves >= 4)
+  //     {
+  //       pastryMoney = Loaves * 2 - (int)(Math.Floor((double)(Loaves / 4))) * 2;
+  //       return pastryMoney;
+  //     }
+  //     return pastryMoney;
+  //   }
+  // }
 
 }
