3f40ff7cd3cee145c1deec1df9e9d9df15639f05
nancyliqn91 <nancyliqn91@gmail.com>
2023-05-05 10:34:39 -0700

    Pass second test

--- BakeryShop/Models/Bakery.cs
+++ BakeryShop/Models/Bakery.cs
@@ -3,19 +3,30 @@ namespace BakeryShop.Models
   public class Bread
   {
     public int Loaves { get; set; }
-    // private int _breadMoney;
-    // Public int BreadMoney
-    // {
-    //   get { return _breadMoney; }
-    // }
+    private int _breadMoney;
+
+    public int BreadMoney
+    {
+      get { return _breadMoney; }
+    }
 
     public Bread(int bNum)
     {
        Loaves = bNum;
     }
 
-    
+    public int BreadOrder()
+    {
+      int breadMoney = 0;
+      if (1 <= Loaves && Loaves< 3)
+      {
+        breadMoney = Loaves * 5;
+        return breadMoney;
+      }
 
+      return breadMoney;
+
+    }
 
   }
 
