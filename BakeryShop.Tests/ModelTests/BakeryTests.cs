732c6726e97e432ccb3b3d6e9bb9ee47a8f9774a
nancyliqn91 <nancyliqn91@gmail.com>
2023-05-05 13:54:23 -0700

    Add first test for abstraction

--- BakeryShop.Tests/ModelTests/BakeryTests.cs
+++ BakeryShop.Tests/ModelTests/BakeryTests.cs
@@ -5,39 +5,13 @@ using System;
 namespace BakeryShop.Tests
 { 
   [TestClass]
-  public class BakeryTests
-  {
-    // for bread
-    [TestMethod]
-    public void BreadConstructor_CreateInstanceOfBread_Bread()
-    {
-      Bread newBread = new Bread(2);
-      Assert.AreEqual(typeof(Bread), newBread.GetType());
-    }
-    
-    [TestMethod]
-    public void BreadOrder_ReturnLessThan3_Int()
-    {
-      Bread newBread = new Bread(2);
-      Assert.AreEqual(10, newBread.BreadOrder());
-    }
-
-    [TestMethod]
-    public void BreadOrder_ReturnMoreThan3_Int()
-    {
-      Bread newBread = new Bread(7);
-      Assert.AreEqual(25, newBread.BreadOrder());
-    }
-  }
-    // for pastry
-  [TestClass]
-  public class PastryTests
+  public class BakeryItemTests
   {
     [TestMethod]
-    public void PastryConstructor_CreateInstanceOfPastry_Pastry()
+    public void BakeryItemConstructor_CreateInstanceOfBakeryItem_BakeryItem()
     {
-      Pastry newPastry = new Pastry(2);
-      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+      BakeryItem newBakeryItem = new BakeryItem(2);
+      Assert.AreEqual(typeof(BakeryItem), newBakeryItem.GetType());
     }
 
     [TestMethod]
@@ -53,7 +27,57 @@ namespace BakeryShop.Tests
       Pastry newPastry = new Pastry(8);
       Assert.AreEqual(12, newPastry.PastryOrder());
     }
-
   }
+
+  // [TestClass]
+  // public class BakeryTests
+  // {
+  //   // for bread
+  //   [TestMethod]
+  //   public void BreadConstructor_CreateInstanceOfBread_Bread()
+  //   {
+  //     Bread newBread = new Bread(2);
+  //     Assert.AreEqual(typeof(Bread), newBread.GetType());
+  //   }
+    
+  //   [TestMethod]
+  //   public void BreadOrder_ReturnLessThan3_Int()
+  //   {
+  //     Bread newBread = new Bread(2);
+  //     Assert.AreEqual(10, newBread.BreadOrder());
+  //   }
+
+  //   [TestMethod]
+  //   public void BreadOrder_ReturnMoreThan3_Int()
+  //   {
+  //     Bread newBread = new Bread(7);
+  //     Assert.AreEqual(25, newBread.BreadOrder());
+  //   }
+  // }
+  //   // for pastry
+  // [TestClass]
+  // public class PastryTests
+  // {
+  //   [TestMethod]
+  //   public void PastryConstructor_CreateInstanceOfPastry_Pastry()
+  //   {
+  //     Pastry newPastry = new Pastry(2);
+  //     Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+  //   }
+
+  //   [TestMethod]
+  //   public void PastryOrder_ReturnLessThan4_Int()
+  //   {
+  //     Pastry newPastry = new Pastry(3);
+  //     Assert.AreEqual(6, newPastry.PastryOrder());
+  //   }
+
+  //   [TestMethod]
+  //   public void PastryOrder_ReturnMoreThan4_Int()
+  //   {
+  //     Pastry newPastry = new Pastry(8);
+  //     Assert.AreEqual(12, newPastry.PastryOrder());
+  //   }
+  // }
 
 }
